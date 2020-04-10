68cdc6afd5c6e0e4562e9bd380f833cd8bac9002
ClawNarok <arthurvgf@gmail.com>
2020-04-10 16:26:09 -0300

    Arrumando movimentação do player + disparo

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,11 +6,9 @@ public class Move : MonoBehaviour
 {
     public CharacterController chtr;
     public Vector3 vMove = Vector3.zero;
-    public float h, v, xPos, zPos, vel = 20;
-    //public float xPos, zPos, vel = 20;
-    public GameObject chaoNovo, chaoAtual;
+    public float h, v, vel = 20;
     public Rigidbody rb;
-    public float slow = 1;
+    public float slow = 0.7f;
     float redutor = 1;
 
     void Awake()
@@ -18,36 +16,32 @@ public class Move : MonoBehaviour
         if (!chtr)
             chtr = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
-        chaoAtual = GameObject.Find("Plane");
     }
 
     void Update()
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
-        // vMove.x = Input.GetAxis("Horizontal");
-        // vMove.y = Input.GetAxis("Vertical");
 
         var pos = transform.position;
         var look = new Vector3(pos.x + h, transform.position.y, pos.z + v);
-        // var look = new Vector3(pos.x + vMove.x, transform.position.y, pos.z + vMove.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
-            transform.position = new Vector3(0, 1, 0);
+        {
+            var bala = Instantiate(Resources.Load<GameObject>("Bala"), transform.position + transform.forward, transform.rotation);
+            bala.transform.GetComponent<Projetil>().Atirador = gameObject;
+            bala.SetActive(true);
+        }
 
         transform.LookAt(look);
-        //print(velMovimento());
         redutor = 1;
         if (Input.GetKey(KeyCode.LeftControl))
             redutor = slow;
-        transform.Translate(Vector3.forward * (velMovimento() * redutor));
-        // print(Input.acceleration.x + "/b" + Input.acceleration.y);
+        transform.Translate(Vector3.forward * (velMovimento() * redutor) * vel * Time.deltaTime);
     }
 
     float velMovimento()
     {
-        // float _h = vMove.x;
-        // float _v = vMove.y;
         float _h = h;
         float _v = v;
 
@@ -56,15 +50,5 @@ public class Move : MonoBehaviour
         if (Mathf.Abs(_v) + Mathf.Abs(_h) >= 1)
             return 1f;
         return Mathf.Abs(_v) + Mathf.Abs(_h);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        print(collision.gameObject.name);
-        if (collision.gameObject != chaoAtual)
-        {
-            chaoAtual = collision.gameObject;
-            chaoNovo = null;
-        }
     }
 }
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+    public GameObject Atirador;
+    Vector3 posInicial;
+    public float velocidade = 3;
+
+    void Start()
+    {
+        posInicial = Atirador.transform.position;
+    }
+
+    void Update()
+    {
+        if ((transform.position - posInicial).magnitude > 20)
+            Destroy(gameObject);
+        else
+            transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+    }
+
+    void OnCollisionEnter (Collision colisor)
+    {
+        print("Bateu porra");
+        if (colisor.gameObject.tag == "Enemy")
+        {
+            Destroy(colisor.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
