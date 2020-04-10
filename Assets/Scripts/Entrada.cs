a72bc2a83d0ad455f7be9019d8b4e891a699dd13
ClawNarok <arthurvgf@gmail.com>
2020-04-10 16:27:26 -0300

    testes de entrada de teclas

---
+++ Assets/Scripts/Entrada.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrada : MonoBehaviour
+{
+    void Start()
+    {
+        var j = Input.GetJoystickNames();
+        for (int x = 0; x < j.Length; x++)
+        {
+            print(j[x]);
+        }
+    }
+
+    void OnGUI()//teste para detectar a entrada do jogador
+    {
+        Event e = Event.current;
+        if (e.isKey)
+            print(e.keyCode);
+    }
+}
