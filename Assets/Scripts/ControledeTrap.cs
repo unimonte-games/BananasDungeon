8ddd7026102e4a6168d10a819becffa8f5976d6c
ClawNarok <arthurvgf@gmail.com>
2020-05-12 01:13:55 -0300

    controle e teste de dano da trap

---
+++ Assets/Scripts/ControledeTrap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControledeTrap : MonoBehaviour
+{
+    public float tempoAtivo;
+    public float tempoDesativado;
+    public GameObject espinhos;
+    int estado = 1;
+
+    void Update()
+    {
+        switch (estado)
+        {
+            case 1:
+                StartCoroutine(Ativar());
+                estado = 0;
+                break;
+            case 2:
+                StartCoroutine(Desativar());
+                estado = 0;
+                break;
+
+        }
+    }
+
+    IEnumerator Ativar()
+    {
+        print("levantando");
+        do
+        {
+            espinhos.transform.position = new Vector3(espinhos.transform.position.x, espinhos.transform.position.y + .1f, espinhos.transform.position.z);
+            yield return new WaitForSeconds(.01f);
+        } while (espinhos.transform.position.y < 0);
+        yield return new WaitForSeconds(tempoAtivo);
+        estado = 2;
+    }
+
+    IEnumerator Desativar()
+    {
+        print("abaixando");
+        do
+        {
+            espinhos.transform.position = new Vector3(espinhos.transform.position.x, espinhos.transform.position.y - .05f, espinhos.transform.position.z);
+            yield return new WaitForSeconds(.01f);
+        } while (espinhos.transform.position.y > -1.15);
+        yield return new WaitForSeconds(tempoDesativado);
+        estado = 1;
+    }
+}
---
+++ Assets/Scripts/DadosPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DadosPlayer : MonoBehaviour
+{
+    public int Vida = 1000;
+    public Slider barraVida;
+
+    void Update()
+    {
+        barraVida.value = Vida;
+    }
+}
