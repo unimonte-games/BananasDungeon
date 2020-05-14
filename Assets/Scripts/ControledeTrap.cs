07174ebabbdcc64d36d0484d8ae5f1c47f9d648d
ClawNarok <arthurvgf@gmail.com>
2020-05-14 19:12:06 -0300

    dano de armadilha de espinhos

--- Assets/Scripts/ControledeTrap.cs
+++ Assets/Scripts/ControledeTrap.cs
@@ -6,6 +6,16 @@ public class ControledeTrap : MonoBehaviour
 {
     public float tempoAtivo;
     public float tempoDesativado;
+    public float velSubida = .1f;
+    public float velDescida = .05f;
+    public float delayDano = 2f;
+    public float danoFixo = 3;
+    public float danoPercentual = 0;
+    public TipoDano Tipo = TipoDano.Fixo;
+    public enum TipoDano
+    {
+        Percentual, Fixo
+    }
     public GameObject espinhos;
     int estado = 1;
 
@@ -21,13 +31,11 @@ public class ControledeTrap : MonoBehaviour
                 StartCoroutine(Desativar());
                 estado = 0;
                 break;
-
         }
     }
 
     IEnumerator Ativar()
     {
-        print("levantando");
         do
         {
             espinhos.transform.position = new Vector3(espinhos.transform.position.x, espinhos.transform.position.y + .1f, espinhos.transform.position.z);
@@ -39,7 +47,6 @@ public class ControledeTrap : MonoBehaviour
 
     IEnumerator Desativar()
     {
-        print("abaixando");
         do
         {
             espinhos.transform.position = new Vector3(espinhos.transform.position.x, espinhos.transform.position.y - .05f, espinhos.transform.position.z);
---
+++ Assets/Scripts/Espinhos.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Espinhos : MonoBehaviour
+{
+    public List<Alvo> Alvos = new List<Alvo>();
+
+    public struct Alvo
+    {
+        public GameObject gbj;
+        public bool dano;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+            Alvo aux = new Alvo();
+            aux.gbj = other.gameObject;
+            aux.dano = true;
+            
+            Alvos.Add(aux);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+            for (int x = 0; x < Alvos.Count; x++)
+            {
+                if (Alvos[x].dano)
+                {
+                    var aux = Alvos[x];
+                    Alvos.Remove(aux);
+                    aux.dano = false;
+                    Alvos.Add(aux);
+                    StartCoroutine(DanoEspinho(aux));
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+            for (int x = 0; x < Alvos.Count; x++)
+            {
+                if (other.gameObject == Alvos[x].gbj)
+                {
+                    Alvos.Remove(Alvos[x]);
+                    break;
+                }
+            }
+        }        
+    }
+
+    IEnumerator DanoEspinho(Alvo alvo)
+    {
+        var atributos = alvo.gbj.GetComponent<Atributos>();
+        if (atributos.Vida > 0)
+        {
+            CalculoDano(alvo, atributos);
+        }
+        yield return new WaitForSeconds(transform.GetComponentInParent<ControledeTrap>().delayDano);
+        var aux = alvo;
+        Alvos.Remove(aux);
+        aux.dano = true;
+        Alvos.Add(aux);
+    }
+
+    public void CalculoDano(Alvo alvo, Atributos atb)
+    {
+        var ctr = gameObject.transform.GetComponentInParent<ControledeTrap>();
+        switch (ctr.Tipo)
+        {
+            case ControledeTrap.TipoDano.Fixo:
+                atb.vidaAtual -= (int)ctr.danoFixo;
+                break;
+            case ControledeTrap.TipoDano.Percentual:
+                print("( ( " + ctr.danoPercentual + " / " + "100" + " ) " +  " * " + atb.Vida + " ) " + " = " + (int)((ctr.danoPercentual / 100) * atb.Vida));
+                print("( ( " + (ctr.danoPercentual / 100) +  " * " + atb.Vida + " ) " + " = " + (int)((ctr.danoPercentual / 100) * atb.Vida));
+                
+                float dano = ((ctr.danoPercentual / 100) * atb.Vida);
+                atb.vidaAtual -= (int)dano;
+                print((int)((ctr.danoPercentual / 100) * atb.Vida));
+                break;
+        }
+    }
+}
