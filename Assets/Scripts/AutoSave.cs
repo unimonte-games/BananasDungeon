f5e026c989626829f9d5cc85f70b91a0b2c69a97
ClawNarok <arthurvgf@gmail.com>
2020-05-28 09:31:09 -0300

    inicio do gerenciamento de save

---
+++ Assets/Scripts/AutoSave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSave : MonoBehaviour
+{
+    public enum Players
+    {
+        Amazona,
+        Oliver
+    }
+    public static List<ArmaNivel> ArmasNiveis = new List<ArmaNivel>();
+    public struct ArmaNivel
+    {
+        public ControleDeArmas.Armas arma;
+        public NivelArma.Nivel nivel;
+    }
+    public static List<PlayerArma> PlayersArmas = new List<PlayerArma>();
+    public struct PlayerArma
+    {
+        public Players player;
+        public ControleDeArmas arma;
+    }
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey(Players.Amazona.ToString()))
+            print("Amazona iniciada");
+
+        if (PlayerPrefs.HasKey(Players.Oliver.ToString()))
+            print("Oliver iniciada");
+    }
+}
