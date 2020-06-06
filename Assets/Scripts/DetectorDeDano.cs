1b1df42866d4a5fd3e9fef7b1b98cd255afb34b7
ClawNarok <arthurvgf@gmail.com>
2020-06-06 20:34:06 -0300

    som de dano

--- Assets/Scripts/DetectorDeDano.cs
+++ Assets/Scripts/DetectorDeDano.cs
@@ -10,6 +10,10 @@ public class DetectorDeDano : MonoBehaviour
     public AudioClip SomDano;
     public AudioSource DisparadordeSons;
 
+    public ControleDeArmas ctrArma;
+    public DanoArma danoArma;
+    public Atributos atb;
+
 
     void Awake()
     {
@@ -19,9 +23,6 @@ public class DetectorDeDano : MonoBehaviour
         Dano = GetComponent<DanoArma>();
     }
 
-    public ControleDeArmas ctrArma;
-    public DanoArma danoArma ;
-    public Atributos atb;
 
     void OnTriggerEnter(Collider other)
     {
@@ -29,25 +30,32 @@ public class DetectorDeDano : MonoBehaviour
         {
             case "Player":
                 print("Player: " + other.tag);
+                if (!Principal.GetComponent<ControleDeAnimacao>().Atacando && GetComponent<NivelArma>().arma != Dados.Armas.Arco && GetComponent<NivelArma>().arma != Dados.Armas.Cajado)
+                    return;
                 if (other.CompareTag("Enemy"))
                 {
                     ControleDeArmas ctrArma = Principal.GetComponent<ControleDeArmas>();
                     DanoArma danoArma = GetComponent<DanoArma>();
                     Atributos atb = other.GetComponent<Atributos>();
                     
+                    DisparadordeSons.PlayOneShot(SomDano);
+
                     int danoCalculado = (int)(danoArma.Dano * ctrArma.multi);
                     int variacao = Random.Range(-5, 6);
                     atb.CausarDano(ctrArma.PegarArma().GetComponent<NivelArma>().arma, danoCalculado + variacao);
+
                 }
                 break;
             case "Enemy":
                 print("Enemy: " + other.tag);
+                if (!Principal.GetComponent<ControledeAnimacaoInimigo>().Atacando)
+                    return;
                 if (other.CompareTag("Player"))
                 {
                     danoArma = GetComponent<DanoArma>();
                     atb = other.GetComponent<Atributos>();
-                    // DanoArma danoArma = GetComponent<DanoArma>();
-                    // Atributos atb = other.GetComponent<Atributos>();
+                    
+                    DisparadordeSons.PlayOneShot(SomDano);
                     
                     int danoCalculado = (int)(danoArma.Dano);
                     int variacao = Random.Range(-5, 6);
